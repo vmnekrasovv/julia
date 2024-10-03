548a13370a6f384175bccc720d0cd1ecd78d994f
vmnekrasovv <vmnekrasov777@gmail.com>
2024-10-04 02:23:25 +0300

    section-offer mobile

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,49 @@
 (function($){
-	$(document).ready(function(){ 
-		
+
+	let wW = window.innerWidth;
+	let slickInitialized = false;
+
+	function slickInit() {
+		$('.offer').slick({
+			speed: 1000,
+			dots: true,
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			autoplay: true,
+			autoplaySpeed: 3000
+		});		
+
+		slickInitialized = true;
+	}
+
+	$(window).on('load', function(){
+
+		wW = window.innerWidth;
+
+		if(wW < 768) {
+			slickInit();
+		} 
 	});
+
+
+	$(window).on('resize', function(){
+
+		wW = window.innerWidth;
+
+		if(wW < 768 && slickInitialized) {
+			$('.offer').slick('refresh');
+		} 
+		else if(wW < 768 && !slickInitialized) {
+			slickInit();
+		}
+		else if(wW > 768 && slickInitialized){
+			$('.offer').slick('unslick');
+		}
+	});
+
+
+
+
 })(jQuery);
